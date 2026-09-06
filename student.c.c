0168c8b64eba104e147b9b6aa2ca80b8dbcf5720
RajdeepDas-20 <rddash275@gmail.com>
2026-09-06 11:16:32 +0600

    Add files via upload

---
+++ student.c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int studentID = 2025331520;
+    float score = 87.5;
+    char grade = 'A';
+
+    printf("%d %f %c", studentID, score, grade);
+    return 0;
+}
+
