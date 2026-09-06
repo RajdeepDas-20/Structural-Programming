e23eff3b9e01ffecd20ce02a35d696cebef5dfcb
RajdeepDas-20 <rddash275@gmail.com>
2026-09-06 16:10:19 +0600

    Add files via upload

---
+++ shipping cost.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int itemPrice = 150; // Declare itemPrice and assign a value
+    int shippingCost = 30; //Declare shippingCost and assign a value
+    int sum; //  Create an int variable named sum
+    sum = itemPrice + shippingCost; //  Calculate total cost and store in sum
+    printf("Total cost: %d\n", sum); // Print the total cost using printf
+
+    return 0;
+}
+
+
+
