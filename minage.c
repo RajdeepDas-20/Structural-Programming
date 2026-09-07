938dc7f3d5dad203973f2bee1d3ecab5a9e9e167
RajdeepDas-20 <rddash275@gmail.com>
2026-09-07 22:55:12 +0600

    Add files via upload

---
+++ minage.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+	int age = 20; // Declare age and assign 20
+    int minAge = 18;// Declare minAge and assign 18
+    printf("%d", age >= minAge); // Check if age is at least minAge
+    return 0; // End the program
+
+}
+
