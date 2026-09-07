03d9b21af88c74d401d868ebb5eb8a061b2a670f
RajdeepDas-20 <rddash275@gmail.com>
2026-09-07 22:57:50 +0600

    Add files via upload

---
+++ age.c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int age = 20; // Declare int variable age and assign a value
+    if (age >= 18) { // Check if age is 18 or older using >= operator
+        printf("Allowed\n");// If true, print Allowed
+    } 
+    else {
+        printf("Not allowed\n");  // Otherwise, print Not allowed
+    }
+
+    return 0;
+
+}
+
