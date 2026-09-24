a8e6aca7eec1d33a83a597bcf24351f801f431c9
RajdeepDas-20 <rddash275@gmail.com>
2026-09-24 23:16:00 +0600

    Add files via upload

---
+++ pointerarr11.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+int main() {
+  int myNumbers[4] = {25, 50, 75, 100};
+  int *ptr = myNumbers;
+  for (int i = 0; i < 4; i++) {
+    printf("%d\n", *(ptr + i));
+  }
+  return 0;
+}
+
