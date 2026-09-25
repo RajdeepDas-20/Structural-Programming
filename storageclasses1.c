f42182a16e62b960dd617d51049af66d1a9b8121
RajdeepDas-20 <rddash275@gmail.com>
2026-09-25 18:48:14 +0600

    Add files via upload

---
+++ storageclasses1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void count() {
+  static int myNum = 0; // Keeps its value between calls
+  myNum++;
+  printf("num = %d\n", myNum);
+}
+
+int main() {
+  count();
+  count();
+  count();
+  return 0;
+}
